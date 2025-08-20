fa8183e3efb27df8d8faf61b9caa20a310c93cff
srushtikulkarni <srushtikulkarni09@gmail.com>
2025-08-20 11:39:07 +0530

    Adding Restaurant schema and seeding new data into restaurant collection

---
+++ BE2.1_HW1/index.js
@@ -0,0 +1,37 @@
+const {initializeDatabase} = require("../db/db.connect");
+const fs = require("fs");
+const Restaurants = require("./models/restaurant.models");
+
+initializeDatabase();
+
+// const jsonData = JSON.parse("")
+
+const newRestaurant = {
+  name: "Cha Cha",
+  cuisine: ["Spanish"],
+  location: "123 Main Street, Anytown",
+  rating: 4.0,
+  reviews: [],
+  website: "https://example.com",
+  phoneNumber: "+1234567890",
+  openHours: "Mon-Sun: 11:00 AM - 10:00 PM",
+  priceRange: "$$ (11-30)",
+  reservationsNeeded: true,
+  isDeliveryAvailable: true,
+  menuUrl: "https://example.com/menu",
+  photos: ["https://example.com/photo1.jpg", "https://example.com/photo2.jpg"],
+};
+
+async function createRestaurant(newRestaurant) {
+    try{
+        const Restaurant = new Restaurants(newRestaurant);
+        const saveRes = await Restaurant.save();
+        console.log(saveRes);
+
+    }catch(error){
+        throw error
+    }
+
+}
+
+createRestaurant(newRestaurant) ;
